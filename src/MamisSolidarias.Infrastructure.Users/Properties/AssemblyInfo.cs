f0e89d3ff39fa09e582b1a952514e49ab6905193
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-08-14 14:01:01 -0300

    [Fix] Allowed Moq to see internal classes

--- src/MamisSolidarias.Infrastructure.Users/Properties/AssemblyInfo.cs
+++ src/MamisSolidarias.Infrastructure.Users/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("MamisSolidarias.WebAPI.Users")]
 [assembly: InternalsVisibleTo("MamisSolidarias.WebAPI.Users.Test")]
 [assembly: InternalsVisibleTo("MamisSolidarias.HttpClient.Users.Test")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
