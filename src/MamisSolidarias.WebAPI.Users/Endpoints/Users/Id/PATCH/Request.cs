4787ba52fbc5a3614d7f0e36211931ea2d791ecc
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-09-08 10:30:42 -0300

    [Fix] Removed broken phone number validation

--- src/MamisSolidarias.WebAPI.Users/Endpoints/Users/Id/PATCH/Request.cs
+++ src/MamisSolidarias.WebAPI.Users/Endpoints/Users/Id/PATCH/Request.cs
@@ -28,17 +28,17 @@ public class RequestValidator : Validator<Request>{
     public RequestValidator()
     {
         RuleFor(t => t.Email)
-            .EmailAddress().WithMessage("Debe ser un email.")
-            .MinimumLength(5).WithMessage("Debe tener como mínimo 5 caracteres.")
-            .MaximumLength(100).WithMessage("Debe tener como máximo 100 caracteres.");
+            .EmailAddress().When(t=> t.Email is not null).WithMessage("Debe ser un email.")
+            .MinimumLength(5).When(t=> t.Email is not null).WithMessage("Debe tener como mínimo 5 caracteres.")
+            .MaximumLength(100).When(t=> t.Email is not null).WithMessage("Debe tener como máximo 100 caracteres.");
 
         RuleFor(t => t.Name)
-            .MinimumLength(5).WithMessage("Debe tener como mínimo 5 caracteres.")
-            .MaximumLength(100).WithMessage("Debe tener como máximo 100 caracteres.");
+            .MinimumLength(5).When(t=> t.Name is not null).WithMessage("Debe tener como mínimo 5 caracteres.")
+            .MaximumLength(100).When(t=> t.Name is not null).WithMessage("Debe tener como máximo 100 caracteres.");
         
         RuleFor(t => t.Phone)
-            .Matches(@"^\+?(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$")
-            .WithMessage("Debe ser un número de teléfono válido.");
+            .MinimumLength(5).When(t=> t.Phone is not null).WithMessage("Debe tener como mínimo 5 caracteres.")
+            .MaximumLength(20).When(t=> t.Phone is not null).WithMessage("Debe tener como máximo 20 caracteres.");
 
         RuleFor(t => t.Id)
             .GreaterThan(0).WithMessage("El Id no es válido");
